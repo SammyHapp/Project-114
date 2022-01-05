b4f4df88e5455f20790d2d58385612c6fa903efb
SammyHapp <86432262+SammyHapp@users.noreply.github.com>
2022-01-04 19:30:17 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function draw(){
+
+}
+
+function setup(){
+    canvas= createCanvas(300 , 300);
+    //ok
+    canvas.center();
+}
+
+function take_snapshot(){
+    save('FunnyLips.png');
+}
